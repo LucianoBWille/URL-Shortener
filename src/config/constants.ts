9b3487808cbdd90e001be20f6c13316fc18ceb0f
LucianoBWille <00lucianobwille00@gmail.com>
2022-03-22 19:16:04 -0300

    first-commit

---
+++ src/config/constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+		'mongodb+srv://luciano:<password>@cluster0.adtc7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+}
