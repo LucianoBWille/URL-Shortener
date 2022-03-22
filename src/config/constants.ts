fa072097de64d4107e79a460bc07ed2cab035040
LucianoBWille <00lucianobwille00@gmail.com>
2022-03-22 20:16:03 -0300

    Ajuste MONGO_CONNECTON e versões de pacotes

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,5 +1,5 @@
 export const config = {
 	API_URL: 'http://localhost:5000',
 	MONGO_CONNECTION:
-		'mongodb+srv://luciano:<password>@cluster0.adtc7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+		`mongodb+srv://luciano:${process.env.PASSWORD}@cluster0.adtc7.mongodb.net/url-shortener-dio?retryWrites=true&w=majority`,
 }
